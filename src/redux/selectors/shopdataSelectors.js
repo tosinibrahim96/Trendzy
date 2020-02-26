49eebd1aef7d5393e8276b66e09ea8962d5405a1
Ibrahim Alausa <tosinibrahim96@gmail.com>
2020-02-26 11:36:37 +0100

    - complete nested route for dhop based on categoryId

--- src/redux/selectors/shopdataSelectors.js
+++ src/redux/selectors/shopdataSelectors.js
@@ -1,7 +1,21 @@
 import { createSelector } from "reselect";
 
+const CATEGORY_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  women: 4,
+  men: 5
+};
+
 const shopData = state => {
   return state.shopData;
 };
 
 export const selectShopPageData = createSelector([shopData], shopData);
+
+export const selectCategoryByUrl = categoryUrlParam => {
+  return createSelector([selectShopPageData], shopData =>
+    shopData.find(category => category.id === CATEGORY_ID_MAP[categoryUrlParam])
+  );
+};
